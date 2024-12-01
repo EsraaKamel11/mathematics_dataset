fbadb8ceb4ffb66b3dfaae961062a332e8f7813b
EsraaKamel11 <Israa.kamel811@gmail.com>
2024-12-01 15:49:26 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,23 +1,3 @@
-# Copyright 2019 DeepMind Technologies Limited.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-"""Module setuptools script."""
-
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-
 from setuptools import find_packages
 from setuptools import setup
 
@@ -45,9 +25,9 @@ setup(
     packages=find_packages(),
     install_requires=[
         'absl-py>=0.1.0',
-        'numpy>=1.10',
+        'numpy>=1.10,<1.25',
         'six',
-        'sympy>=1.2',
+        'sympy>=1.2,<1.10',
     ],
     classifiers=[
         'Development Status :: 4 - Beta',
@@ -56,11 +36,10 @@ setup(
         'Intended Audience :: Science/Research',
         'License :: OSI Approved :: Apache Software License',
         'Operating System :: POSIX :: Linux',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
         'Topic :: Scientific/Engineering :: Artificial Intelligence',
     ],
 )
